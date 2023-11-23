b78c024f83c7dda1f03f76c81a42ba52e2e2d078
Beatriz Alves <bcalves3@stefanini.com>
2023-11-23 16:05:04 -0300

    alterando comunicacao entre front e back

--- frontend/src/pages/user/ViewPage.vue
+++ frontend/src/pages/user/ViewPage.vue
@@ -270,7 +270,8 @@ export default {
 	mixins: [accountMixin],
     data() {
         return {
-            baseURL: process.env.VUE_APP_API_URL,
+            //baseURL: 'http://api-sedese.stefanini.test/api/v1/',
+            baseURL: 'http://200.198.62.82:8082/api/v1/',
             UserId: this.$route.params.id,
             user: [],
             contact: [],
