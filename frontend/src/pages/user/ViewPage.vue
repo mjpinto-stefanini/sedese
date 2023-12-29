94766e3a5e8f10e426e584747f53a6471287a485
Beatriz Alves <bcalves3@stefanini.com>
2023-12-29 10:11:28 -0300

    devolvendo as configuracoes da master para o merge

--- frontend/src/pages/user/ViewPage.vue
+++ frontend/src/pages/user/ViewPage.vue
@@ -270,8 +270,8 @@ export default {
 	mixins: [accountMixin],
     data() {
         return {
-            baseURL: 'http://api-sedese.stefanini.test/api/v1/',
-            //baseURL: 'http://200.198.62.82/api/v1/',
+            //baseURL: 'http://api-sedese.stefanini.test/api/v1/',
+            baseURL: 'http://200.198.62.82/api/v1/',
             UserId: this.$route.params.id,
             user: [],
             contact: [],
